20fda5aac977cc3de8f9f0963720ab8ea93afac2
Michael McGahan <mike.mcgahan@gmail.com>
2016-09-07 10:00:15 +1200

    loads of comments and clearer var names

--- middleware/sync.js
+++ middleware/sync.js
@@ -23,15 +23,19 @@ import { activeRouteQueries$ } from '../util/routeUtils';
 import { fetchQueries } from '../util/fetchUtils';
 
 /**
- * Stateful function to handle a subscription that should not
+ * Stateful higher-order function to handle a subscription that should not
  * have parallel instances.
+ *
+ * The next/error/completed handlers initially supplied will be applied to each
+ * subsequent Observable stream passed to the returned function
  */
-const serialSubscription = (next, error, completed) => {
-	let serialSub = new Rx.Subscription();
+const makeSerialSubscriber = (next, error, completed) => {
+	let serialSub = new Rx.Subscription();  // empty sub on init
 	return obs$ => {
 		if (!serialSub.isUnsubscribed) {
-			serialSub.unsubscribe();
+			serialSub.unsubscribe();  // kill any open subscriptions
 		}
+		// set up the new subscription
 		serialSub = obs$.subscribe(
 			next,
 			error,
@@ -49,24 +53,28 @@ const serialSubscription = (next, error, completed) => {
  * will make necessary calls to the API
  */
 const getSyncMiddleware = routes => store => {
-	/**
-	 * We want to make sure there is always and only one data-fetching subscription
-	 * open at a time, so we create an empty subscription that is scoped to the
-	 * request. When a new fetch happens, this subscription will get replaced with
-	 * a new subscription.
-	 */
 	const actions = bindActionCreators(
 		{ apiRequest, apiSuccess, apiError, apiComplete, locationSync },
 		store.dispatch
 	);
 
-	const doFetch = serialSubscription(
+	/*
+	 * We want to make sure there is always and only one data-fetching subscription
+	 * open at a time, so we create a 'serial subscriber' that can be passed an
+	 * Observable to subscribe to with a particular set of next/error/completed
+	 * handlers. When a new fetch happens, this subscription will get replaced with
+	 * a new subscription.
+	 */
+	const subscribeToFetch = makeSerialSubscriber(
 		actions.apiSuccess,
 		actions.apiError,
 		actions.apiComplete
 	);
 
+	// Now, return the curried functions used by all Redux middleware
 	return next => action => {
+		// CASE 1: navigation-related actions - gather reactive query info and
+		// dispatch an API_REQUEST action
 		if (action.type === LOCATION_CHANGE ||  // client nav
 			action.type === '@@server/RENDER' ||
 			action.type === 'LOCATION_SYNC') {
@@ -76,15 +84,21 @@ const getSyncMiddleware = routes => store => {
 				.delay(0)  // needed in order for LOCATION_CHANGE to finish processing
 				.filter(queries => queries);  // only emit value if queries exist
 
+			// we have our stream of queries, now dispatch an action with them
 			activeQueries$.subscribe(actions.apiRequest);
 		}
 
+		// CASE 2: Any time auth information changes, we need to re-load any app
+		// data related to the current location to reflect what the new auth allows
+		// the client to see
 		if (action.type === 'CONFIGURE_AUTH' && !action.meta) {
 			setTimeout(() => {
 				actions.locationSync(store.getState().routing.locationBeforeTransitions);
 			}, 0);
 		}
 
+		// CASE 3: When there's an API_REQEUST, go ahead and make the fetch call
+		// and use subscribeToFetch to manage the subscription
 		if (action.type === 'API_REQUEST') {
 			const {
 				auth,
@@ -92,10 +106,10 @@ const getSyncMiddleware = routes => store => {
 			} = store.getState();
 			// should read auth from cookie if on browser, only read from state if on server
 
-			const apiFetch$ = Rx.Observable.of(action.payload)
-				.flatMap(fetchQueries(config.apiUrl, { method: 'GET', auth }));
+			const apiFetch$ = Rx.Observable.of(action.payload)  // read the queries
+				.flatMap(fetchQueries(config.apiUrl, { method: 'GET', auth }));  // call fetch
 
-			doFetch(apiFetch$);
+			subscribeToFetch(apiFetch$);  // open the subscription
 		}
 
 		return next(action);
