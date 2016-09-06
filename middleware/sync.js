dfbf0271cc4bd4673f32b23743708d23007aa66d
Michael McGahan <mike.mcgahan@gmail.com>
2016-09-06 14:44:53 +1200

    serial subscription function

--- middleware/sync.js
+++ middleware/sync.js
@@ -23,12 +23,23 @@ import { activeRouteQueries$ } from '../util/routeUtils';
 import { fetchQueries } from '../util/fetchUtils';
 
 /**
- * We want to make sure there is always and only one data-fetching subscription
- * open at a time, so we create an empty subscription that is scoped to the
- * request. When a new fetch happens, this subscription will get replaced with
- * a new subscription.
+ * Stateful function to handle a subscription that should not
+ * have parallel instances.
  */
-let apiFetchSub = new Rx.Subscription();
+const serialSubscription = (next, error, completed) => {
+	let serialSub = new Rx.Subscription();
+	return obs$ => {
+		if (!serialSub.isUnsubscribed) {
+			serialSub.unsubscribe();
+		}
+		serialSub = obs$.subscribe(
+			next,
+			error,
+			completed
+		);
+		return serialSub;
+	};
+};
 
 /**
  * The middleware is exported as a getter because it needs the application's
@@ -37,52 +48,58 @@ let apiFetchSub = new Rx.Subscription();
  * The middleware itself - passes the queries to the application server, which
  * will make necessary calls to the API
  */
-const getSyncMiddleware = routes => store => next => action => {
-	if (action.type === LOCATION_CHANGE ||  // client nav
-		action.type === '@@server/RENDER' ||
-		action.type === 'LOCATION_SYNC') {
-		const dispatchApiRequest = bindActionCreators(apiRequest, store.dispatch);
+const getSyncMiddleware = routes => store => {
+	/**
+	 * We want to make sure there is always and only one data-fetching subscription
+	 * open at a time, so we create an empty subscription that is scoped to the
+	 * request. When a new fetch happens, this subscription will get replaced with
+	 * a new subscription.
+	 */
+	const actions = bindActionCreators(
+		{ apiRequest, apiSuccess, apiError, apiComplete, locationSync },
+		store.dispatch
+	);
 
-		const location = action.payload;
-		const activeQueries$ = activeRouteQueries$(routes, { location })
-			.delay(0)  // needed in order for LOCATION_CHANGE to finish processing
-			.filter(queries => queries);  // only emit value if queries exist
+	const doFetch = serialSubscription(
+		actions.apiSuccess,
+		actions.apiError,
+		actions.apiComplete
+	);
 
-		activeQueries$.subscribe(dispatchApiRequest);
-	}
+	return next => action => {
+		if (action.type === LOCATION_CHANGE ||  // client nav
+			action.type === '@@server/RENDER' ||
+			action.type === 'LOCATION_SYNC') {
 
-	if (action.type === 'CONFIGURE_AUTH' && !action.meta) {
-		setTimeout(() => {
-			store.dispatch(locationSync(store.getState().routing.locationBeforeTransitions));
-		}, 0);
-	}
+			const location = action.payload;
+			const activeQueries$ = activeRouteQueries$(routes, { location })
+				.delay(0)  // needed in order for LOCATION_CHANGE to finish processing
+				.filter(queries => queries);  // only emit value if queries exist
 
-	if (action.type === 'API_REQUEST') {
-		const actions = bindActionCreators(
-			{ apiSuccess, apiError, apiComplete },
-			store.dispatch
-		);
-		const {
-			auth,
-			config,
-		} = store.getState();
-		// should read auth from cookie if on browser, only read from state if on server
+			activeQueries$.subscribe(actions.apiRequest);
+		}
 
-		const apiFetch$ = Rx.Observable.of(action.payload)
-			.flatMap(fetchQueries(config.apiUrl, { method: 'GET', auth }));
+		if (action.type === 'CONFIGURE_AUTH' && !action.meta) {
+			setTimeout(() => {
+				actions.locationSync(store.getState().routing.locationBeforeTransitions);
+			}, 0);
+		}
 
-		// Before creating a new subscription, we need to destroy any
-		// existing subscriptions
-		apiFetchSub.unsubscribe();
-		// Now, set up the subscription to the new fetch
-		apiFetchSub = apiFetch$.subscribe(
-			actions.apiSuccess,
-			actions.apiError,
-			actions.apiComplete
-		);
-	}
+		if (action.type === 'API_REQUEST') {
+			const {
+				auth,
+				config,
+			} = store.getState();
+			// should read auth from cookie if on browser, only read from state if on server
 
-	return next(action);
+			const apiFetch$ = Rx.Observable.of(action.payload)
+				.flatMap(fetchQueries(config.apiUrl, { method: 'GET', auth }));
+
+			doFetch(apiFetch$);
+		}
+
+		return next(action);
+	};
 };
 
 export default getSyncMiddleware;
