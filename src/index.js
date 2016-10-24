d3551b17627aa6f3655bea3fd5ca0b85fb729fc1
Michael McGahan <mike.mcgahan@gmail.com>
2016-10-25 11:17:33 +1300

    add mocks to root export

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import makeServerRender from './renderers/server-render';
 import makeBrowserRender from './renderers/browser-render';
 import startServer from './renderers/browser-render';
+import * as apiMocks from './util/mocks/api';
+import * as appMocks from './util/mocks/app';
 
 module.exports = {
 	makeServerRender,
 	makeBrowserRender,
-	startServer
+	startServer,
+	mocks: {
+		app: appMocks,
+		api: apiMocks,
+	}
 };
 
