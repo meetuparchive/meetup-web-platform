30a308ef2759ae595e90427e7288da04cf444477
Michael McGahan <mike.mcgahan@gmail.com>
2016-10-25 11:05:27 +1300

    index file

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import makeServerRender from './renderers/server-render';
+import makeBrowserRender from './renderers/browser-render';
+import startServer from './renderers/browser-render';
+
+module.exports = {
+	makeServerRender,
+	makeBrowserRender,
+	startServer
+};
+
