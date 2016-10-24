bad9a50e2a0747de04b2365599affacd42a04186
Michael McGahan <mike.mcgahan@gmail.com>
2016-10-25 11:57:13 +1300

    import from the right place, please

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // IMPORTANT: only export server-side modules here - browser
 // should only import modules individually
 import makeServerRenderer from './renderers/server-render';
-import startServer from './renderers/browser-render';
+import startServer from './server';
 import * as apiMocks from './util/mocks/api';
 import * as appMocks from './util/mocks/app';
 
