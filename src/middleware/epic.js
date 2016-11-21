452814cc86233a0d3255e6e13c46e419f930fe27
Michael McGahan <mike.mcgahan@gmail.com>
2016-11-22 09:18:59 +1300

    note about server epics

--- src/middleware/epic.js
+++ src/middleware/epic.js
@@ -9,6 +9,10 @@ import getPostEpic from '../epics/post';
  * The middleware is exported as a getter because it needs the application's
  * routes in order to set up the nav-related epic(s) that are part of the
  * final middleware
+ *
+ * **Note** it's unlikely that the server needs any epics other than `sync` in
+ * order to render the application. We may want to write a server-specific
+ * middleware that doesn't include the other epics if performance is an issue
  */
 const getPlatformMiddleware = (routes, fetchQueries) => createEpicMiddleware(
 	combineEpics(
