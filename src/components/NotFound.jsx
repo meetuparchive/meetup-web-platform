89dddb6403c133096256bbca301655c484d961ff
Michael McGahan <mike.mcgahan@gmail.com>
2017-01-07 14:38:25 +1300

    docs

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -4,10 +4,16 @@ import withSideEffect from 'react-side-effect';
 /**
  * Use this component as a wrapper for your 'not found' UI when the API returns
  * no data
+ *
+ * Note that only one child is allowed for this component since it does not
+ * provide any rendered output itself
  */
 class NotFound extends React.Component {
 	render() {
-		return React.Children.only(this.props.children);
+		if (this.props.children) {
+			return React.Children.only(this.props.children);
+		}
+		return null;
 	}
 }
 
