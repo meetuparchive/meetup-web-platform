15867dd9c9b324b5cb92fce83eec1a26ff906802
Niko Lazaris <niko@meetup.com>
2017-01-18 15:44:10 -0500

    updated linkify function just to be Better Than Before

--- src/util/linkify.js
+++ src/util/linkify.js
@@ -1,28 +1,34 @@
 
+const URL_REGEX = /((((ht|f){1}(tps?:[/][/]){1}))[-a-zA-Z0-9@:;%_\+.~#?&/=]+)/gm;
+
+/**
+ * Generates HTML link tag element with target
+ *
+ * @method createLink
+ * @param {Object} options optional modifiers
+ * @param {String} href string of link passed through options
+ * @return {String} The HTML link tag
+ */
+const createLink = options => href => {
+	const target = options.target || '';
+	const targetAttr = `target="${target}"`;
+	const relAttr = target === '_blank' ? 'rel="noopener noreferrer"' : '';
+
+	return `<a href="${href}" title="${href}" ${targetAttr} ${relAttr}>${href}</a>`;
+}
+
 /**
  * Replaces URLs in a block of text with HTML anchor tags.
  *
  * @method linkify
  * @param {String} text The text on which to operate
  * @param {Object} options optional modifiers
- * @param {Boolean} [options.target] what to set as the target attribute
  * @return {String} The modified text.
  */
 export default function linkify(text, options) {
-	if (!text) return '';
-	options = options || {};
+	if (!text) {
+		return '';
+	}
 
-	const urlRegex = /((((ht|f){1}(tps?:[/][/]){1}))[-a-zA-Z0-9@:;%_\+.~#?&/=]+)/gm,
-		createLink = (href) => {
-			const target = options.target || '',
-				targetAttr = `target="${target}"`;
-
-			if (target === '_blank') {
-				targetAttr += ' rel="noopener noreferrer"';
-			}
-
-			return `<a href="${href}" title="${href}" ${targetAttr}>${href}</a>`;
-		};
-
-	return text.replace(urlRegex, createLink);
+	return text.replace(URL_REGEX, createLink(opitions || {}));
 }
