4b63a3df073825f50422bbf364bb288cccd8e74d
Michael McGahan <mike.mcgahan@gmail.com>
2017-01-21 07:25:57 +1300

    testing

---
+++ src/util/clickTracking.test.js
@@ -0,0 +1,15 @@
+describe('trackStopPropagation', () => {
+	it('adds Event.prototype.stopPropAndTrack', () => {
+	});
+	it('overrides Event.prototype.stopPropagation', () => {
+	});
+	describe('stopPropAndTrack', () => {
+		it('calls trackClick', () => {
+		});
+	});
+	describe('stopPropagation', () => {
+		it('calls stopPropAndTrack for click events', () => {
+		});
+	});
+});
+
