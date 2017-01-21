28e6e00676c085518290c390a6b7ed42b5ed0923
Michael McGahan <mike.mcgahan@gmail.com>
2017-01-21 13:11:03 +1300

    tests in progress

--- src/util/clickTracking.test.js
+++ src/util/clickTracking.test.js
@@ -1,7 +1,15 @@
+import getTrackClick, * as clickTracking from './clickTracking';
 describe('trackStopPropagation', () => {
 	it('adds Event.prototype.stopPropAndTrack', () => {
+		const spyable = {
+			dummyListener() {}
+		};
+		spyOn(spyable, 'dummyListener');
+		clickTracking.trackStopPropagation(spyable.dummyListener);
+		expect(Event.prototype.stopPropAndTrack).toBeDefined();
 	});
 	it('overrides Event.prototype.stopPropagation', () => {
+		expect(Event.prototype.stopPropAndTrack).not.toBeDefined();
 	});
 	describe('stopPropAndTrack', () => {
 		it('calls trackClick', () => {
@@ -10,6 +18,12 @@ describe('trackStopPropagation', () => {
 	describe('stopPropagation', () => {
 		it('calls stopPropAndTrack for click events', () => {
 		});
+	});
+});
+
+describe('getTrackClick', () => {
+	it('calls trackStopPropagation', () => {
+		getTrackClick();
 	});
 });
 
