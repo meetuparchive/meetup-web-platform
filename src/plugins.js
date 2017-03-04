ba90be57f4438e4c4fbea95ae34ac93923fa10d0
Michael McGahan <mike.mcgahan@gmail.com>
2017-03-04 13:28:19 +1300

    documentation, better layout

--- src/plugins.js
+++ src/plugins.js
@@ -17,22 +17,39 @@ export function setCsrfCookies(request, reply) {
 	return reply.continue();
 }
 
+/**
+ * The CSRF plugin we use - 'electrode-csrf-jwt' compares a cookie token to a
+ * header token in non-GET requests. By default, it will set the cookie token
+ * itself ('x-csrf-jwt'), and supply the corresponding header token in a custom
+ * header (also 'x-csrf-jwt'). However, we update this flow to also supply the
+ * header token as a cookie ('x-csrf-jwt-header') so that it syncs across
+ * browser tabs.
+ *
+ * In order to ensure that both cookie values have parallel settings, this
+ * function calls `server.state` for both cookie names before registering the
+ * plugin.
+ *
+ * @param {String} secret the 'salt' for encoding the CSRF tokens
+ * @return {Object} the { register, options } object for a `server.register` call.
+ */
 export function getCsrfPlugin(secret) {
 	const register = (server, options, next) => {
 		const cookieOptions = {
 			path: '/',
 			isSecure: process.env.NODE_ENV === 'production',
 		};
-		server.state('x-csrf-jwt', {
-			...cookieOptions,
-			isHttpOnly: true,
-		});  // set by plugin
-		server.state('x-csrf-jwt-header', {
-			...cookieOptions,
-			isHttpOnly: false,
-		});  // set by onPreResponse
+		server.state(
+			'x-csrf-jwt',  // set by plugin
+			{ ...cookieOptions, isHttpOnly: true }  // no client-side interaction needed
+		);
+		server.state(
+			'x-csrf-jwt-header',  // set by onPreResponse
+			{ ...cookieOptions, isHttpOnly: false } // the client must read this cookie and return as a custom header
+		);
+
 		const registration = CsrfPlugin.register(server, options, next);
-		server.ext('onPreResponse', setCsrfCookies);  // must add this extension _after_ plugin is registered
+		server.ext('onPreResponse', setCsrfCookies);  // this extension must be registered _after_ plugin is registered
+
 		return registration;
 	};
 	register.attributes = CsrfPlugin.register.attributes;
