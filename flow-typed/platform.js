ed7c6234f7c0c8d7e18566a53b50b1139f66b1a6
Michael McGahan <mike.mcgahan@gmail.com>
2017-04-21 10:34:41 +1200

    declare instead of export

--- flow-typed/platform.js
+++ flow-typed/platform.js
@@ -1,17 +1,17 @@
 // @flow
 declare var Intl: Object;
 
-export type FluxStandardAction = {
+declare type FluxStandardAction = {
 	type: string,
 	payload?: any,
 	meta?: any,
 	error?: boolean
 };
 
-export type Reducer = (state: Object, action: FluxStandardAction) => Object;
+declare type Reducer = (state: Object, action: FluxStandardAction) => Object;
 
 // API query structure
-export type Query = {
+declare type Query = {
 	ref: string,
 	endpoint: string,
 	params?: Object,
@@ -22,7 +22,7 @@ export type Query = {
 	},
 }
 
-export type QueryResponse = {
+declare type QueryResponse = {
 	ref: string,
 	value: Object | Array<Object>,
 	type?: string,
