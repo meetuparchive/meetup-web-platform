3c5151a665ad7645f3cbc9e41dd85b6850ba525b
Matt Kime <matt@mattki.me>
2017-04-25 21:38:56 -0500

    fix import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // IMPORTANT: only export server-side modules here - browser
 // should only import modules individually
 import makeServerRenderer from './renderers/server-render';
-import makeRenderer$ as makeServerRenderer$ from './renderers/server-render';
+import { makeRenderer$ as makeServerRenderer$ } from './renderers/server-render';
 import startServer from './server';
 import * as apiMocks from 'meetup-web-mocks/lib/api';
 import * as appMocks from 'meetup-web-mocks/lib/app';
