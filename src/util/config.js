566ba02ac6a8714ba959d0ed6b8121785af5fe6f
Michael McGahan <mike.mcgahan@gmail.com>
2017-05-10 12:19:19 +1200

    log the config in tests

--- src/util/config.js
+++ src/util/config.js
@@ -217,5 +217,6 @@ config.set(
 config.set('isProd', config.get('env') === 'production');
 config.set('isDev', config.get('env') === 'development');
 config.validate();
+process.stdout.write(JSON.stringify(config.getProperties(), null, 2));
 
 export default config.getProperties();
