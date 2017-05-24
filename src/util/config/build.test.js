3abe94f56fb807fa8af87587d5820060ab9c3dc4
Michael McGahan <mike.mcgahan@gmail.com>
2017-05-25 07:33:53 +1200

    tests

--- src/util/config/build.test.js
+++ src/util/config/build.test.js
@@ -1,4 +1,4 @@
-import { PROTOCOL_ERROR, validateProtocol } from './build';
+import { PROTOCOL_ERROR, validateProtocol, validateServerHost } from './build';
 
 describe('validateProtocol', () => {
 	it('does not error when protocol is `http` or `https`', () => {
@@ -8,5 +8,29 @@ describe('validateProtocol', () => {
 
 	it('throws error when the protocol is not `http` or `https`', () => {
 		expect(() => validateProtocol('ftp')).toThrowError(PROTOCOL_ERROR);
+	});
+});
+
+describe('validateServerHost', () => {
+	it('throws an error for non-string values', () => {
+		expect(() => validateServerHost(null)).toThrow();
+		expect(() => validateServerHost(1234)).toThrow();
+		expect(() => validateServerHost({ foo: 'bar' })).toThrow();
+		expect(() => validateServerHost(['foo'])).toThrow();
+		expect(() => validateServerHost('foo')).not.toThrow();
+	});
+	it('throws error for dev in prod', () => {
+		const _env = process.env.NODE_ENV; // cache the 'real' value to restore later
+		process.env.NODE_ENV = 'production';
+		expect(() => validateServerHost('foo.dev.bar.com')).toThrow();
+		expect(() => validateServerHost('foo.bar.com')).not.toThrow();
+		process.env.NODE_ENV = _env; // restore original env value
+	});
+	it('throws error for dev in prod', () => {
+		const _env = process.env.NODE_ENV; // cache the 'real' value to restore later
+		process.env.NODE_ENV = 'development';
+		expect(() => validateServerHost('foo.dev.bar.com')).not.toThrow();
+		expect(() => validateServerHost('foo.bar.com')).toThrow();
+		process.env.NODE_ENV = _env; // restore original env value
 	});
 });
