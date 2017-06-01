1c93b8645c60a01b79b711cd99ed12127aaaad16
Michael McGahan <mike.mcgahan@gmail.com>
2017-06-02 09:23:24 +1200

    move asset_server back to build config

--- src/util/config/build.js
+++ src/util/config/build.js
@@ -7,6 +7,25 @@ import path from 'path';
  */
 
 export const schema = {
+	// asset server is needed for dev build in order to set up HMR server
+	asset_server: {
+		host: {
+			format: String,
+			default: 'beta2.dev.meetup.com',
+			env: 'ASSET_SERVER_HOST',
+		},
+		path: {
+			format: String,
+			default: '/static',
+			env: 'ASSET_PATH',
+		},
+		port: {
+			format: 'port',
+			default: 8001, // must be 443 for prod
+			arg: 'asset-port',
+			env: process.env.NODE_ENV !== 'test' && 'ASSET_SERVER_PORT', // don't read env in tests
+		},
+	},
 	env: {
 		format: ['production', 'development', 'test'],
 		default: 'development',
@@ -27,6 +46,18 @@ export const schema = {
 	},
 };
 export const config = convict(schema);
+
+const configPath = path.resolve(
+	process.cwd(),
+	`config.${config.get('env')}.json`
+);
+
+export const { asset_server } = fs.existsSync(configPath)
+	? require(configPath)
+	: {};
+if (asset_server) {
+	config.load({ asset_server });
+}
 
 config.set('isProd', config.get('env') === 'production');
 config.set('isDev', config.get('env') === 'development');
