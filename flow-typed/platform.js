f326e5378236e7275291b97b3e9d403cdaf3d7d9
Michael McGahan <mike.mcgahan@gmail.com>
2017-07-04 12:27:31 +1200

    back to spaces

--- flow-typed/platform.js
+++ flow-typed/platform.js
@@ -4,90 +4,90 @@ declare var Intl: Object;
 declare type Params = { [string]: string };
 
 declare type FluxStandardAction = {
-	type: string,
-	payload?: any,
-	meta?: any,
-	error?: boolean,
+  type: string,
+  payload?: any,
+  meta?: any,
+  error?: boolean,
 };
 
 declare type Reducer = (state: ?Object, action: FluxStandardAction) => Object;
 
 // API query structure
 declare type Query = {
-	ref: string,
-	endpoint: string,
-	params?: Params,
-	type?: string,
-	meta?: {
-		flags?: Array<string>,
-		method?: string,
-	},
+  ref: string,
+  endpoint: string,
+  params?: Params,
+  type?: string,
+  meta?: {
+    flags?: Array<string>,
+    method?: string,
+  },
 };
 
 declare type QueryResponse = {
-	ref: string,
-	value: Object | Array<Object>,
-	type?: string,
-	flags?: Array<string>,
-	meta?: Object,
+  ref: string,
+  value: Object | Array<Object>,
+  type?: string,
+  flags?: Array<string>,
+  meta?: Object,
 };
 
 declare type QueryFunction = (location: { [string]: mixed }) => Query;
 
 declare type PlatformRoute = {
-	component: ReactClass<*>,
-	getNestedRoutes?: () => Promise<Array<PlatformRoute>>,
-	getIndexRoute?: () => Promise<PlatformRoute>,
-	path?: string,
-	exact?: boolean,
-	strict?: boolean,
-	query?: QueryFunction | Array<QueryFunction>,
-	indexRoute?: PlatformRoute,
-	routes?: Array<PlatformRoute>,
+  component: ReactClass<*>,
+  getNestedRoutes?: () => Promise<Array<PlatformRoute>>,
+  getIndexRoute?: () => Promise<PlatformRoute>,
+  path?: string,
+  exact?: boolean,
+  strict?: boolean,
+  query?: QueryFunction | Array<QueryFunction>,
+  indexRoute?: PlatformRoute,
+  routes?: Array<PlatformRoute>,
 };
 
 declare type CookieOpts = {
-	path?: string,
-	isHttpOnly?: boolean,
-	isSecure?: boolean,
-	encoding?: string,
+  path?: string,
+  isHttpOnly?: boolean,
+  isSecure?: boolean,
+  encoding?: string,
 };
 
 declare type Match = {
-	params: Params,
-	isExact: boolean,
-	path: string,
-	url: string,
+  params: Params,
+  isExact: boolean,
+  path: string,
+  url: string,
 };
 
 declare type MatchedRoute = { route: PlatformRoute, match: Match };
 declare type MatchPathOptions = {
-	path: string,
-	exact?: boolean,
-	strict?: boolean,
+  path: string,
+  exact?: boolean,
+  strict?: boolean,
 };
 
 declare module 'react-router-dom/matchPath' {
-	declare function matchPath(
-		pathname: string,
-		options: MatchPathOptions
-	): null | Match;
-	declare module.exports: typeof matchPath;
+  declare function matchPath(
+    pathname: string,
+    options: MatchPathOptions
+  ): null | Match;
+  declare module.exports: typeof matchPath;
 }
 
 declare type LocationShape = {
-	pathname?: string,
-	search?: string,
-	hash?: string,
-	state?: any,
+  pathname?: string,
+  search?: string,
+  hash?: string,
+  state?: any,
 };
 
 declare class RouterRedirect extends React$Component {
-	props: {
-		to: string | LocationShape,
-		push?: boolean,
-	},
+  props: {
+    to: string | LocationShape,
+    push?: boolean,
+  },
 }
 declare module 'react-router-dom/Redirect' {
-	declare export default typeof RouterRedirect
+  declare export default typeof RouterRedirect
 }
