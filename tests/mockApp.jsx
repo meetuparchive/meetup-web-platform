faebfd2b942bad0a18cc11a570b6d0f6b72536a8
Michael McGahan <mike.mcgahan@gmail.com>
2017-07-20 14:57:16 +1200

    force boolean on call to 'reply().permanent()'

--- tests/mockApp.jsx
+++ tests/mockApp.jsx
@@ -7,17 +7,28 @@ export const assetPublicPath = '//whatever';
 export const reducer = makeRootReducer();
 
 export const ROOT_INDEX_CONTENT = 'this is the life';
-const MockRootIndex = props => <div>{ROOT_INDEX_CONTENT}</div>;
+const MockRootIndex = props =>
+	<div>
+		{ROOT_INDEX_CONTENT}
+	</div>;
 export const FOO_INDEX_CONTENT = 'yo dawg i heard you like foo';
-const MockFooIndex = props => <div>{FOO_INDEX_CONTENT}</div>;
+const MockFooIndex = props =>
+	<div>
+		{FOO_INDEX_CONTENT}
+	</div>;
 export const EXTERNAL_REDIRECT_URL = 'http://example.com/foo?return=foo';
 export const INTERNAL_REDIRECT_PATH = '/foo';
 const MockRedirect = props => {
-	const to = props.match.params.redirectType === 'internal'
-		? INTERNAL_REDIRECT_PATH
-		: new URL(EXTERNAL_REDIRECT_URL);
-	const permanent = Boolean(props.match.params.isPermanent);
-	return <div><Redirect to={to} permanent={permanent} /></div>;
+	const to =
+		props.match.params.redirectType === 'internal'
+			? INTERNAL_REDIRECT_PATH
+			: new URL(EXTERNAL_REDIRECT_URL);
+	const permanent = props.match.params.isPermanent;
+	return (
+		<div>
+			<Redirect to={to} permanent={permanent} />
+		</div>
+	);
 };
 
 export const routes = [
