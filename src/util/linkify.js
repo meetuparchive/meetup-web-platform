47c5cfe6d6ba7a51dc7622cb96e1807bc55b051b
Jackie Jimenez <jaclynejimenez@gmail.com>
2017-08-15 15:43:48 -0400

    add comment to show where the regex came from

--- src/util/linkify.js
+++ src/util/linkify.js
@@ -1,4 +1,6 @@
 // @flow
+
+// Regex to match urls, copied from url-regex v3. Modified to not match closing parantheses ')' at the end of the url string
 var urlRegex = new RegExp(
 	/(?:(?:(?:[a-z]+:)?\/\/)|www\.)(?:\S+(?::\S*)?@)?(?:localhost|(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])(?:\.(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])){3}|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#][^\s)"]*)?/gi
 );
