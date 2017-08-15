aa8831d51847ec8cef0c7bda4a63675c455cbc20
Jackie Jimenez <jaclynejimenez@gmail.com>
2017-08-15 14:24:39 -0400

    update regex (copied from url-regex, modified to not select closing parentheses); remove url-regex package

--- src/util/linkify.js
+++ src/util/linkify.js
@@ -1,5 +1,7 @@
 // @flow
-import urlRegex from 'url-regex';
+var urlRegex = new RegExp(
+	/(?:(?:(?:[a-z]+:)?\/\/)|www\.)(?:\S+(?::\S*)?@)?(?:localhost|(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])(?:\.(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])){3}|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#][^\s)"]*)?/gi
+);
 
 /**
  * Generates HTML link tag element with target
@@ -31,6 +33,5 @@ export default function linkify(text: string, options?: Object = {}): string {
 	if (!text) {
 		return '';
 	}
-
-	return text.replace(urlRegex(), createLink(options));
+	return text.replace(urlRegex, createLink(options));
 }
