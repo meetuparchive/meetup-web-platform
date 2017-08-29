e04f5d3cf1b9f29b332da399ee1e21aa9c1d7b06
Michael McGahan <mike.mcgahan@gmail.com>
2017-08-29 21:32:47 +0100

    add utils

---
+++ src/app-server/util/index.js
@@ -0,0 +1,124 @@
+import https from 'https';
+import Hapi from 'hapi';
+import uuid from 'uuid';
+
+/**
+ * determine whether a nested object of values has
+ * a string that contains `.dev.meetup.`
+ *
+ * @param {String|Object} value string or nested object
+ * with values that could be URL strings
+ *
+ * @return {Boolean} whether the `value` contains a 'dev' URL string
+ */
+export function checkForDevUrl(value) {
+	switch (typeof value) {
+		case 'string':
+			return value.indexOf('.dev.meetup.') > -1;
+		case 'object':
+			return Object.keys(value).some(key => checkForDevUrl(value[key]));
+	}
+
+	return false;
+}
+
+export function onRequestExtension(request, reply) {
+	request.id = uuid.v4();
+
+	request.server.app.logger.info(
+		`Incoming request ${request.method.toUpperCase()} ${request.url.href}`
+	);
+
+	return reply.continue();
+}
+
+export function logResponse(request) {
+	const {
+		method,
+		response,
+		id,
+		info,
+		server: { app: { logger } },
+		url,
+	} = request;
+
+	if (response.isBoom) {
+		// response is an Error object
+		logger.error(`Internal error ${response.message} ${url.pathname}`, {
+			error: response.stack,
+		});
+	}
+
+	const log = ((response.statusCode >= 400 && logger.error) ||
+		(response.statusCode >= 300 && logger.warn) ||
+		logger.info)
+		.bind(logger);
+
+	log(
+		{
+			headers: response.headers,
+			id,
+			method,
+			referrer: info.referrer,
+			remoteAddress: info.remoteAddress,
+			time: info.responded - info.received,
+			href: url.href,
+		},
+		`Outgoing response ${method.toUpperCase()} ${url.pathname} ${response.statusCode}`
+	);
+
+	return;
+}
+
+/**
+ * Use server.ext to add functions to request/server extension points
+ * @see {@link https://hapijs.com/api#request-lifecycle}
+ * @param {Object} server Hapi server
+ * @return {Object} Hapi server
+ */
+export function registerExtensionEvents(server) {
+	server.ext([
+		{
+			type: 'onRequest',
+			method: onRequestExtension,
+		},
+	]);
+	server.on('response', logResponse);
+	return server;
+}
+
+/**
+ * Make any environment changes that need to be made in response to the provided
+ * config
+ *
+ * @param {Object} config the environment configuration object
+ *
+ * @return null
+ */
+export function configureEnv(config) {
+	// When using .dev.meetup endpoints, ignore self-signed SSL cert
+	const USING_DEV_ENDPOINTS = checkForDevUrl(config);
+
+	https.globalAgent.options.rejectUnauthorized = !USING_DEV_ENDPOINTS;
+}
+
+/**
+ * server-starting function
+ */
+export function server(routes, connection, plugins, config) {
+	const server = new Hapi.Server();
+
+	// store runtime state - must modify existing server.settings.app in order to keep
+	// previously-defined properties
+	// https://hapijs.com/api#serverapp
+	server.settings.app = Object.assign(server.settings.app || {}, config);
+
+	const appConnection = server.connection(connection);
+	return appConnection
+		.register(plugins)
+		.then(() => registerExtensionEvents(server))
+		.then(() => server.auth.strategy('default', 'oauth', true))
+		.then(() => appConnection.route(routes))
+		.then(() => server.start())
+		.then(() => server);
+}
---
+++ src/app-server/util/index.test.js
@@ -0,0 +1,117 @@
+import convict from 'convict';
+import https from 'https';
+import { MOCK_LOGGER, getServer } from '../../util/testUtils';
+import * as serverUtils from './';
+
+describe('checkForDevUrl', () => {
+	it('returns true for dev URLs', () => {
+		expect(serverUtils.checkForDevUrl('www.dev.meetup.com')).toBe(true);
+		expect(serverUtils.checkForDevUrl('secure.dev.meetup.com')).toBe(true);
+	});
+
+	it('rejects non-dev URLs and non-objects', () => {
+		expect(serverUtils.checkForDevUrl('www.meetup.com')).toBe(false);
+		expect(serverUtils.checkForDevUrl('secure.meetup.com')).toBe(false);
+		expect(serverUtils.checkForDevUrl(1234)).toBe(false);
+		expect(serverUtils.checkForDevUrl(true)).toBe(false);
+		expect(serverUtils.checkForDevUrl(new Date())).toBe(false);
+	});
+
+	it('returns true for nested dev URLs', () => {
+		expect(serverUtils.checkForDevUrl({ url: 'www.dev.meetup.com' })).toBe(
+			true
+		);
+		expect(
+			serverUtils.checkForDevUrl({
+				url1: 'www.meetup.com',
+				url2: 'www.dev.meetup.com',
+			})
+		).toBe(true);
+		expect(
+			serverUtils.checkForDevUrl({ url: { url: 'www.dev.meetup.com' } })
+		).toBe(true);
+	});
+});
+
+describe('configureEnv', function() {
+	beforeEach(() => {
+		// cache the 'default' setting for rejectUnauthorized
+		this.defaultRejectUnauthorized =
+			https.globalAgent.options.rejectUnauthorized;
+	});
+
+	afterEach(function() {
+		// restore the default setting for rejectUnauthorized
+		https.globalAgent.options.rejectUnauthorized = this.defaultRejectUnauthorized;
+	});
+
+	it('sets global rejectUnauthorized to false when using dev URLs in config', () => {
+		serverUtils.configureEnv(
+			convict({
+				url: 'www.dev.meetup.com',
+			})
+		);
+		expect(https.globalAgent.options.rejectUnauthorized).toBe(false);
+	});
+
+	it('sets global rejectUnauthorized to true when using prod URLs in config', () => {
+		serverUtils.configureEnv(
+			convict({
+				url: 'www.meetup.com',
+			})
+		);
+		expect(https.globalAgent.options.rejectUnauthorized).toBe(true);
+	});
+});
+
+describe('onRequestExtension', () => {
+	const request = {
+		headers: 'foo',
+		id: 'bar',
+		method: 'get',
+		info: {},
+		url: { href: 'http://example.com' },
+		state: {},
+		server: getServer(),
+	};
+
+	it('calls reply.continue', () => {
+		const reply = {
+			continue: () => {},
+		};
+		spyOn(reply, 'continue');
+		serverUtils.onRequestExtension(request, reply);
+		expect(reply.continue).toHaveBeenCalled();
+	});
+
+	it('calls console.log with request headers and id', () => {
+		const reply = {
+			continue: () => {},
+		};
+		MOCK_LOGGER.info.mockClear();
+		serverUtils.onRequestExtension(request, reply);
+		const calledWith = MOCK_LOGGER.info.mock.calls[0][0];
+		expect(calledWith).toEqual(expect.stringContaining(request.url.href));
+	});
+});
+
+describe('logResponse', () => {
+	const request = {
+		headers: 'foo',
+		id: 'bar',
+		method: 'get',
+		info: {},
+		url: {},
+		response: {
+			headers: { foo: 'bar' },
+		},
+		server: getServer(),
+	};
+
+	it('calls console.log with response headers and request id', () => {
+		MOCK_LOGGER.info.mockClear();
+		serverUtils.logResponse(request);
+		const [data] = MOCK_LOGGER.info.mock.calls[0];
+		expect(data).toEqual(expect.any(Object));
+	});
+});
