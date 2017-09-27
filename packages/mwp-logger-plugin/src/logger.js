9d09b7d1e93df5c3d229d85a0194cfbc28a32819
Michael McGahan <mike.mcgahan@gmail.com>
2017-09-28 07:25:21 +1300

    more complete logging resource spec

--- packages/mwp-logger-plugin/src/logger.js
+++ packages/mwp-logger-plugin/src/logger.js
@@ -2,15 +2,19 @@ import bunyan from 'bunyan';
 import LoggingBunyan from '@google-cloud/logging-bunyan';
 
 const streams = [{ stream: process.stdout }]; // stdout by default
-if (process.env.GAE_INSTANCE) {
-	console.log('Activating GAE logging');
+const { GAE_INSTANCE, GCLOUD_PROJECT, GAE_VERSION, GAE_SERVICE } = process.env;
+if (GAE_INSTANCE) {
+	console.log(
+		`Activating GAE logging for ${GCLOUD_PROJECT} - ${GAE_SERVICE} v${GAE_VERSION}`
+	);
 	streams.push(
 		LoggingBunyan({
 			resource: {
 				type: 'gae_app',
 				labels: {
-					project_id: process.env.GCLOUD_PROJECT,
-					version_id: process.env.GAE_VERSION,
+					project_id: GCLOUD_PROJECT,
+					module_id: GAE_SERVICE,
+					version_id: GAE_VERSION,
 				},
 			},
 		}).stream()
