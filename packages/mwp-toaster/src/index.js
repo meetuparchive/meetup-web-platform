14450e236b9f5dbd7284368487f3a1547e373c98
Michael McGahan <mike.mcgahan@gmail.com>
2017-10-13 06:51:08 +1300

    readme, remove dead import

--- packages/mwp-toaster/src/index.js
+++ packages/mwp-toaster/src/index.js
@@ -1,3 +1,3 @@
 export { default as reducer } from './reducer';
 export { default as ToastContainer } from './ToastContainer';
-export { makeToast, showToast, dismissToast } from './actions';
+export { makeToast, showToasts } from './actions';
