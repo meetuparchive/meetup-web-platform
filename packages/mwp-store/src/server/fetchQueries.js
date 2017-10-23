683cede9623bca5428a032b12d24c00b18295f2e
Michael McGahan <mike.mcgahan@gmail.com>
2017-10-24 11:50:54 +1300

    use cookie for member on server, redux in browser

--- packages/mwp-store/src/server/fetchQueries.js
+++ packages/mwp-store/src/server/fetchQueries.js
@@ -1,15 +1,25 @@
 // @flow
+import querystring from 'querystring';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/toPromise';
 import { parseQueryResponse, getAuthedQueryFilter } from '../util/fetchUtils';
 
+const isProd = process.env.NODE_ENV === 'production';
+const getCookieMember = memberCookie => {
+	const memberObj = querystring.parse(memberCookie);
+	memberObj.id = memberObj.id ? parseInt(memberObj.id, 10) : 0;
+	return memberObj;
+};
 /**
  * on the server, we can proxy the API requests directly without making a
  * request to the server's own API proxy endpoint
  */
-export default (request: HapiRequest) => (apiUrl, member) => (
+export default (request: HapiRequest) => () => (
 	queries: Array<Query>
 ): Promise<ParsedQueryResponses> => {
+	const member = getCookieMember(
+		request.state[isProd ? 'MEETUP_MEMBER' : 'MEETUP_MEMBER_DEV']
+	);
 	const authedQueries = getAuthedQueryFilter(member);
 	const validQueries = queries.filter(authedQueries);
 	return request
