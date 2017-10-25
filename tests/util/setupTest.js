f2ba9c3036b9c5ede2f0f3a2c44ff063c9b3fefb
Michael McGahan <mike.mcgahan@gmail.com>
2017-10-25 14:34:45 +1300

    update packages

--- tests/util/setupTest.js
+++ tests/util/setupTest.js
@@ -1,5 +1,5 @@
 require('./globals');
 const Enzyme = require('enzyme');
-const Adapter = require('enzyme-adapter-react-15');
+const Adapter = require('enzyme-adapter-react-16');
 
 Enzyme.configure({ adapter: new Adapter() });
