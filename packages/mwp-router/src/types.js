73ac6e4a9c795fe26e4e91494b3654d95cc04b70
Andy Stanberry <astanberry@meetup.com>
2017-11-08 13:34:44 -0500

    Match PropType

---
+++ packages/mwp-router/src/types.js
@@ -0,0 +1,14 @@
+import {
+  shape,
+  bool,
+  string,
+  object,
+} from "prop-types";
+
+// https://reacttraining.com/react-router/web/api/match
+export const Match = shape({
+  isExact: bool,
+  params: object.isRequired,
+  path: string.isRequired,
+  url: string.isRequired,
+});
