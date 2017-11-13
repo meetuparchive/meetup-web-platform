de640223b524fcc46a4cb29ed77e554a5e637e22
Michael McGahan <mike.mcgahan@gmail.com>
2017-11-13 14:39:13 +1300

    pathname not path

--- packages/mwp-core/src/renderers/server-render.jsx
+++ packages/mwp-core/src/renderers/server-render.jsx
@@ -239,7 +239,7 @@ const makeRenderer = (
 			supportedLangs,
 			initialNow: new Date().getTime(),
 			variants: getVariants(state),
-			entryPath: url.path,
+			entryPath: url.pathname,
 		},
 	};
 
