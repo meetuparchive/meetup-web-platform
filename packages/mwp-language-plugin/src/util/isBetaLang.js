9722597bbca10811bacd07a8cd7a1b90e068ebe8
will h <whoward@meetup.com>
2017-12-11 11:15:36 -0500

    Comment to bump beta version.

--- packages/mwp-language-plugin/src/util/isBetaLang.js
+++ packages/mwp-language-plugin/src/util/isBetaLang.js
@@ -1,3 +1,4 @@
+// Beta languages should not redirect traffic to language folders automatically.
 const isBetaLang = lang => {
 	const betaLangs = ['ru-RU', 'ru'];
 	return betaLangs.includes(lang);
