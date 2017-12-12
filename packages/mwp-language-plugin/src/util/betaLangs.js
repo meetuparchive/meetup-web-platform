fa405e4f41293da790739266f8e92e7f0c840c0a
will h <whoward@meetup.com>
2017-12-12 17:54:55 -0500

    RM warns.

--- packages/mwp-language-plugin/src/util/betaLangs.js
+++ packages/mwp-language-plugin/src/util/betaLangs.js
@@ -2,13 +2,10 @@ export const BETA_LANGS = ['ru-RU', 'ru'];
 
 export const isBetaLang = (lang, testBetaLangs) => {
 	const betaLangs = testBetaLangs ? testBetaLangs : BETA_LANGS;
-	console.warn(testBetaLangs);
-	console.warn(betaLangs);
 	return betaLangs.includes(lang);
 };
 
 export const getNotBetaLangs = (langs, testBetaLangs) => {
 	const betaLangs = testBetaLangs ? testBetaLangs : BETA_LANGS;
-	console.warn(betaLangs);
 	return langs.filter(lang => betaLangs.indexOf(lang) === -1);
 };
