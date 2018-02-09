ee0eb5744da066b80e973fabd95070ecf686b82d
Jason <jasonh@meetup.com>
2018-02-09 11:38:37 -0700

    Add a few more tests

--- packages/mwp-app-render/src/components/seoHead.test.jsx
+++ packages/mwp-app-render/src/components/seoHead.test.jsx
@@ -4,7 +4,6 @@ import { SEOHeadComponent } from './SEOHead';
 
 const MOCK_PROPS = {
 	baseUrl: 'https://baseUrl.com',
-	cardType: 'mock-card-type',
 	imageUrl: 'https://www.mock-image-url.com/',
 	ldJson: [{ foo: 'MOCK_LD_JSON' }, { bar: 'MOCK_LD_JSON' }],
 	localeCode: 'es-ES',
@@ -21,6 +20,33 @@ const renderComponent = (props = MOCK_PROPS) => shallow(<SEOHeadComponent {...pr
 describe('SEOHead', () => {
 	it('renders component markup', () => {
 		expect(renderComponent()).toMatchSnapshot();
+	});
+	describe('robots', () => {
+		const robotsSelector = 'meta[name="robots"]';
+		it('defaults to robots "on"', () => {
+			const props = {...MOCK_PROPS};
+			delete props.robots;
+			expect(renderComponent(props).find(robotsSelector)).toMatchSnapshot();
+		});
+		it('explicity turns robots "on"', () => {
+			const props = {...MOCK_PROPS, robots: true};
+			delete props.robots;
+			expect(renderComponent(props).find(robotsSelector)).toMatchSnapshot();
+		});
+		it('turns robots "off"', () => {
+			const props = {...MOCK_PROPS, robots: false};
+			expect(renderComponent(props).find(robotsSelector)).toMatchSnapshot();
+		});
+	});
+	describe('Custom Open Graph data', () => {
+		const ogDescriptionSelector = 'meta[property="og:description"]';
+		it('renders pageDescription as open graph description', () => {
+			expect(renderComponent().find(ogDescriptionSelector)).toMatchSnapshot();
+		});
+		it('renders custom open graph description', () => {
+			const props = {...MOCK_PROPS, ogDescription: 'mock custom open graph description' };
+			expect(renderComponent(props).find(ogDescriptionSelector)).toMatchSnapshot();
+		});
 	});
 	describe('imageUrl prop usage', () => {
 		describe('og:image tag', () => {
