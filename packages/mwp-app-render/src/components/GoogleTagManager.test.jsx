bcac2bc34658cb23154a34a3306c48692050ee49
Jason <jasonh@meetup.com>
2018-03-09 15:36:14 -0700

    GoogleTagManager component

---
+++ packages/mwp-app-render/src/components/GoogleTagManager.test.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import GoogleTagManager from './GoogleTagManager';
+
+const shallowRender = (props) => {
+	return shallow(<GoogleTagManager {...props} />);
+};
+
+describe('google tag manager', () => {
+	it('returns null if rtf is false', () => {
+		expect(shallowRender({rtf: false})).toMatchSnapshot();
+	});
+	describe('script', () => {
+		const props = {
+			rtf: true,
+			tag: 'script',
+		};
+		it('matches snap prod environment', () => {
+			expect(shallowRender({...props, isProd: true})).toMatchSnapshot();
+		});
+		it('matches snap for dev environment', () => {
+			expect(shallowRender({...props, isProd: false})).toMatchSnapshot();
+		});
+	});
+	describe('noscript', () => {
+		const props = {
+			rtf: true,
+			tag: 'noscript',
+		}; 
+		it('matches snap prod environment', () => {
+			expect(shallowRender({...props, isProd: true})).toMatchSnapshot();
+		});
+		it('matches snap for dev environment', () => {
+			expect(shallowRender({...props, isProd: false})).toMatchSnapshot();
+		});
+	});
+});
