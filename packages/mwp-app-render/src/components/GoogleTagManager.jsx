d6a54db1a7afd40e2cd9dc6ee99a508d68a2a963
Jason <jasonh@meetup.com>
2018-03-14 12:22:09 -0600

    GTM updates

--- packages/mwp-app-render/src/components/GoogleTagManager.jsx
+++ packages/mwp-app-render/src/components/GoogleTagManager.jsx
@@ -1,36 +1,29 @@
 // @flow
 
 import React from 'react';
-import PropTypes from 'prop-types';
 
 // Production and dev keys
 const GTM_KEY = process.env.NODE_ENV === 'production' ? 'GTM-T2LNGD' : 'GTM-W9W847';
 
 /*
- * @description Gets google tag manager script tag
+ * @description Gets google tag manager JS snippet
  * @see {@link https://developers.google.com/tag-manager/quickstart}
 */
-export const GoogleTagManagerScript = (key : string = GTM_KEY) => {
-	const script = `
-		(function(w,d,s,l,i){
-			w[l]=w[l]||[];
-			w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
-			var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-			j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-			f.parentNode.insertBefore(j,f);
-		})(window,document,'script','dataLayer','${GTM_KEY}');`
-	return <script dangerouslySetInnerHTML={{ __html: script }}></script>
-};
+export const getGoogleTagManagerSnippet = (): string => (
+	`(function(w,d,s,l,i){
+		w[l]=w[l]||[];
+		w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
+		var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+		j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+		f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','${GTM_KEY}');`
+);
 
 /*
  * @description Gets google tag manager noscript tag
  * @see {@link https://developers.google.com/tag-manager/quickstart}
 */
-export const GoogleTagManagerNoscript = (key : string  = GTM_KEY) => {
-	const iframe = `
-		<iframe
-			src="https://www.googletagmanager.com/ns.html?id=${GTM_KEY}"
-			height="0" width="0" style="display:none;visibility:hidden">
-		</iframe>`;
-	return <noscript dangerouslySetInnerHTML={{ __html: iframe }}></noscript>
+export const GoogleTagManagerNoscript = () => {
+	const src = `https://www.googletagmanager.com/ns.html?id=${GTM_KEY}`;
+	return <noscript><iframe src={src} height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe></noscript>;
 };
