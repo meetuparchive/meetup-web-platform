b2e3ce9cd502206c64ef617270ff37b529ada88a
Adam Detrick <akdetrick@gmail.com>
2018-03-22 10:46:21 -0400

    add inlineStyle util test for unpopulated styles

--- packages/mwp-core/src/util/inlineStyle.test.js
+++ packages/mwp-core/src/util/inlineStyle.test.js
@@ -2,7 +2,7 @@ import { getInlineStyleTags } from './inlineStyle.js';
 
 // `simple-universal-style-loader` reades from `global`,
 // so we must mock the data directly in `global.__styles__`
-global.__styles__ = [
+const MOCK_GLOBAL_STYLES = [
 	{
 		id: 666,
 		parts: [
@@ -16,7 +16,12 @@ global.__styles__ = [
 
 describe('getInlineStyleTags', () => {
 	it('matches snapshot', () => {
+		global.__styles__ = MOCK_GLOBAL_STYLES;
 		const result = getInlineStyleTags()[0];
 		expect(result).toMatchSnapshot();
+		global.__styles__ = null;
+	});
+	it('does not throw when global styles are not populated', () => {
+		expect(() => getInlineStyleTags()).not.toThrow();
 	});
 });
