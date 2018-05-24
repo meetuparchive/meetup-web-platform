46e205d1bb457a0391f53d6f2c455b5d05ddf0b1
Mike Perrotti <mikep@meetup.com>
2018-05-24 15:59:21 -0400

    extend base config in swarmProject config

--- packages/mwp-config/stylelint/stylelint-config-swarmProject.js
+++ packages/mwp-config/stylelint/stylelint-config-swarmProject.js
@@ -1,4 +1,5 @@
 module.exports = {
+	extends: './stylelint-config-base',
 	rules: {
 		'selector-class-pattern': [
 			'^(?:[a-z]+[a-zA-Z0-9]+_)*(?:[a-z]+[a-zA-Z0-9]+)*(?:-[a-zA-Z0-9]+)*(?:--[a-zA-Z0-9]+)?$',
