9c7af29149588ff9238b88bf79ccc29c45fa0b35
Michael McGahan <mike.mcgahan@gmail.com>
2018-06-15 14:10:07 -0400

    StateBroadcast component to publish state

---
+++ packages/mwp-app-render/src/components/StateBroadcast.jsx
@@ -0,0 +1,20 @@
+// @flow
+import React from 'react';
+import { connect } from 'react-redux';
+
+type Props = {
+	state: MWPState,
+};
+const mapStateToProps = state => ({ state });
+
+class StateBroadCastComponent extends React.PureComponent<Props> {
+	componentDidMount() {
+		window.getAppState = () => this.props.state;
+	}
+	render() {
+		// don't actually need to render - this.props.state will always contain
+		// latest Redux state
+	}
+}
+
+export default connect(mapStateToProps, () => ({}))(StateBroadCastComponent);
