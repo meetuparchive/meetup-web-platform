322eb18c8a5848eb8c8c0919aebf622c6edae167
Andrey Eliseyev <aeliseyev@meetup.com>
2018-08-09 17:50:22 +0300

    Make sort not necessary param in merge object

--- packages/mwp-api-state/src/reducer.js
+++ packages/mwp-api-state/src/reducer.js
@@ -63,8 +63,13 @@ export const getListState: ResponseStateSetter = (
 	const oldList = ((state[dynamicRef] || {}).value || [])
 		.filter(valOld => !newList.find(valNew => idTest(valOld, valNew)));
 
+	const mergedList = [...oldList, ...newList];
+	// we can omit sort callback e.g. in members search default sort is `closest_match` which we decided not to implement on FE side
+	if (sort) {
+		mergedList.sort(sort);
+	}
 	// combine the new list and the old list and sort the results
-	return { [dynamicRef]: { value: [...oldList, ...newList].sort(sort) } };
+	return { [dynamicRef]: { value: mergedList } };
 };
 
 export const responseToState: ResponseStateSetter = (
