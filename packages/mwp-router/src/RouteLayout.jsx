a17686aaef33c6704ff7d4e9340550336b122d9e
Michael McGahan <mike.mcgahan@gmail.com>
2018-08-24 10:18:35 +1200

    route layout should not be purecomponent

--- packages/mwp-router/src/RouteLayout.jsx
+++ packages/mwp-router/src/RouteLayout.jsx
@@ -1,38 +1,38 @@
+// @flow
 import PropTypes from 'prop-types';
 import React from 'react';
 import Switch from 'react-router-dom/Switch';
 import Route from 'react-router-dom/Route';
 import AsyncRoute from './AsyncRoute';
 
-/**
- * @class RouteLayout
- */
-class RouteLayout extends React.PureComponent {
-	render() {
-		const { routes, matchedPath = '/' } = this.props;
+type Props = {|
+	routes: Array<PlatformRoute>,
+	matchedPath: string,
+|};
+const RouteLayout = (props: Props) => {
+	const { routes, matchedPath = '/' } = props;
 
-		return (
-			<Switch>
-				{routes.map(route => {
-					const path =
-						matchedPath === '/' // root path, no need to prepend
-							? route.path
-							: `${matchedPath}${route.path || ''}`;
+	return (
+		<Switch>
+			{routes.map(route => {
+				const path =
+					matchedPath === '/' // root path, no need to prepend
+						? route.path
+						: `${matchedPath}${route.path || ''}`;
 
-					return (
-						<Route
-							key={`${path}${route.exact}`}
-							path={path}
-							exact={route.exact || false}
-							strict={route.strict || false}
-							render={props => <AsyncRoute {...props} route={route} />}
-						/>
-					);
-				})}
-			</Switch>
-		);
-	}
-}
+				return (
+					<Route
+						key={`${path || 'empty'}-${route.exact ? 'exact' : 'false'}`}
+						path={path}
+						exact={route.exact || false}
+						strict={route.strict || false}
+						render={props => <AsyncRoute {...props} route={route} />}
+					/>
+				);
+			})}
+		</Switch>
+	);
+};
 
 RouteLayout.propTypes = {
 	routes: PropTypes.arrayOf(PropTypes.object).isRequired,
