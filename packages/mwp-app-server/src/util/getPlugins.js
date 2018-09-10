29a6876298f98ede516af2c4872cbf66950ba36a
Daniel Hayes <dhayes@meetup.com>
2018-09-10 15:03:19 -0400

    add blob to csp plugin options

--- packages/mwp-app-server/src/util/getPlugins.js
+++ packages/mwp-app-server/src/util/getPlugins.js
@@ -159,7 +159,7 @@ export default function getPlugins({ languageRenderers }) {
 			].join(' '),
 			connectSrc: '*',
 			frameSrc: '*',
-			imgSrc: '* data:',
+			imgSrc: '* data: blob:',
 			styleSrc: ['*', CSP_KEYWORDS.unsafeInline].join(' '),
 			scriptSrc: ['*', CSP_KEYWORDS.unsafeEval, CSP_KEYWORDS.unsafeInline].join(
 				' '
