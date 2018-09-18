e8c596762377ec8b41fd99cbcf5425599af4cdb3
Sadaf Shahsahebi <sadaf@meetup.com>
2018-09-18 09:59:45 -0700

    copy files from mup-web to mwp-app-render

---
+++ packages/mwp-app-render/src/components/uxcapture/UXCaptureInlineMark.jsx
@@ -0,0 +1,19 @@
+// @flow
+import React from 'react';
+
+type Props = {
+	mark: string,
+};
+
+const generateUXCaptureJS = (mark: string) => `
+	<script>
+		if(window.UX) {
+			UX.mark("${mark}");
+		}
+	</script>
+`;
+
+// inject inline UX.mark() call into rendered markup
+// @see https://github.com/meetup/ux-capture#text-without-custom-font
+export default ({ mark }: Props) =>
+	<div dangerouslySetInnerHTML={{ __html: generateUXCaptureJS(mark) }} />; // eslint-disable-line react/no-danger
