ead002229f6eb7d66cdf5adfaeb8563a64c13eaf
Sadaf Shahsahebi <sadaf@meetup.com>
2018-10-08 07:57:47 -0700

    PR: guard pattern

--- packages/mwp-app-render/src/components/uxcapture/UXCapture.jsx
+++ packages/mwp-app-render/src/components/uxcapture/UXCapture.jsx
@@ -11,39 +11,43 @@ type Props = {|
 |};
 
 export const onMark = (mark: string) => {
-	if (window.newrelic) {
-		const now = new Date().getTime();
+	if (!window.newrelic) {
+		return;
+	}
 
-		// Set a marker in the trace details
-		window.newrelic.addToTrace({
-			name: mark,
-			start: now,
-			type: 'UX Capture mark',
-		});
+	const now = new Date().getTime();
 
-		//  Add a custom attribute to the PageView & BrowserInteraction events in Insights
-		if (window.performance) {
-			// `window performance.timing.navigationStart` is the event that NR uses as 'start' of page load
-			const timeToMark = now - window.performance.timing.navigationStart;
-			window.newrelic.setCustomAttribute(mark, timeToMark);
-		}
+	// Set a marker in the trace details
+	window.newrelic.addToTrace({
+		name: mark,
+		start: now,
+		type: 'UX Capture mark',
+	});
+
+	//  Add a custom attribute to the PageView & BrowserInteraction events in Insights
+	if (window.performance) {
+		// `window performance.timing.navigationStart` is the event that NR uses as 'start' of page load
+		const timeToMark = now - window.performance.timing.navigationStart;
+		window.newrelic.setCustomAttribute(mark, timeToMark);
 	}
 };
 
 export const onMeasure = (measure: string) => {
-	if (window.newrelic) {
-		const now = new Date().getTime();
+	if (!window.newrelic) {
+		return;
+	}
 
-		window.newrelic.addToTrace({
-			name: measure,
-			start: now,
-			type: 'UX Capture measure',
-		});
+	const now = new Date().getTime();
 
-		if (window.performance) {
-			const timeToMeasure = now - window.performance.timing.navigationStart;
-			window.newrelic.setCustomAttribute(measure, timeToMeasure);
-		}
+	window.newrelic.addToTrace({
+		name: measure,
+		start: now,
+		type: 'UX Capture measure',
+	});
+
+	if (window.performance) {
+		const timeToMeasure = now - window.performance.timing.navigationStart;
+		window.newrelic.setCustomAttribute(measure, timeToMeasure);
 	}
 };
 
