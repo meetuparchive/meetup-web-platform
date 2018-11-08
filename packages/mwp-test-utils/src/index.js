d8b48bb73ef485a871ccfd721e7406f9705663d6
Sadaf Shahsahebi <sadaf@meetup.com>
2018-11-08 14:48:34 -0800

    move testCreateStore() from mwp-test-utils to mwp-store/src/util/testUtils.js

--- packages/mwp-test-utils/src/index.js
+++ packages/mwp-test-utils/src/index.js
@@ -73,21 +73,3 @@ export async function getServer() {
 
 	return server;
 }
-
-const IDENTITY_REDUCER = state => state;
-export function testCreateStore(createStoreFn) {
-	it('creates a store with store functions', () => {
-		const basicStore = createStoreFn(IDENTITY_REDUCER);
-		expect(basicStore.getState).toEqual(jasmine.any(Function));
-		expect(basicStore.dispatch).toEqual(jasmine.any(Function));
-	});
-	it('creates a store with supplied initialState', done => {
-		const initialState = { foo: 'bar' };
-		const basicStore = createStoreFn(IDENTITY_REDUCER, initialState);
-		basicStore.subscribe(() => {
-			expect(basicStore.getState()).toEqual(initialState);
-			done();
-		});
-		basicStore.dispatch({ type: 'dummy' });
-	});
-}
