201e0345e39912d1df51e33475401165e625b062
Michael McGahan <mike.mcgahan@gmail.com>
2018-12-10 08:31:05 +1300

    basic class refactor for inline mark

--- packages/mwp-app-render/src/components/uxcapture/UXCaptureInlineMark.jsx
+++ packages/mwp-app-render/src/components/uxcapture/UXCaptureInlineMark.jsx
@@ -5,17 +5,49 @@ type Props = {
 	mark: string,
 };
 
-const generateUXCaptureJS = (mark: string) => `
-	<script>
-		if(window.UXCapture) {
-			window.UXCapture.mark("${mark}");
-		}
-	</script>
+const markJsString = (mark: string) => `
+	if(window.UXCapture) {
+		window.UXCapture.mark("${mark}");
+	}
 `;
 
+const clearMark = (mark: string) => {
+	if (window.UXCapture) {
+		window.UXCapture.clearMark(this.props.mark);
+	}
+};
+const recordMark = (mark: string) => {
+	if (window.UXCapture) {
+		window.UXCapture.mark(mark);
+	}
+};
 // inject inline UXCapture.mark() call into rendered markup
 // @see https://github.com/meetup/ux-capture#text-without-custom-font
-const UXCaptureInlineMark = ({ mark }: Props) =>
-	<div dangerouslySetInnerHTML={{ __html: generateUXCaptureJS(mark) }} />; // eslint-disable-line react/no-danger
+class UXCaptureInlineMark extends React.Component<Props> {
+	// when mounting on client, only call mark if mark does not already exist
+	componentDidMount() {
+		recordMark(this.props.mark);
+	}
+	// updated on client - if mark name has changed, clear old mark and trigger new
+	componentDidUpdate(prevProps: Props) {
+		const { mark } = this.props;
+		if (prevProps.mark !== mark) {
+			clearMark(prevProps.mark);
+			recordMark(mark);
+		}
+	}
+	// clear associated mark
+	componentWillUnmount() {
+		clearMark(this.props.mark);
+	}
+	render() {
+		<div
+			// eslint-disable-next-line react/no-danger
+			dangerouslySetInnerHTML={{
+				__html: `<script>${markJsString(this.props.mark)}</script>`,
+			}}
+		/>;
+	}
+}
 
 export default UXCaptureInlineMark;
