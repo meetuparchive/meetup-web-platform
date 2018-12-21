36751c8c4177c50c69cc0c26aeec23916fd93dbe
Michael McGahan <mike.mcgahan@gmail.com>
2018-12-21 14:04:06 +1300

    simplify to reduce diff

--- packages/mwp-app-render/src/components/uxcapture/UXCaptureInlineMark.jsx
+++ packages/mwp-app-render/src/components/uxcapture/UXCaptureInlineMark.jsx
@@ -9,14 +9,14 @@ type Props = {
 // inject inline UXCapture.mark() call into rendered markup, in addition to
 // UXCaptureInteractiveMark behavior
 // @see https://github.com/meetup/ux-capture#text-without-custom-font
-const UXCaptureInlineMark = (props: Props) =>
-	<UXCaptureInteractiveMark mark={props.mark}>
+const UXCaptureInlineMark = ({ mark }: Props) =>
+	<UXCaptureInteractiveMark mark={mark}>
 		<div
 			// eslint-disable-next-line react/no-danger
 			dangerouslySetInnerHTML={{
 				__html: `<script>
 					if(window.UXCapture) {
-						window.UXCapture.mark("${props.mark}");
+						window.UXCapture.mark("${mark}");
 					}
 				</script>`,
 			}}
