e1a5113898578b745c3a1119043a8cb909d64d54
mmcgahan <mike.mcgahan@gmail.com>
2019-05-04 11:49:38 +1200

    chore: remove unused test coverage config

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,4 @@
 module.exports = {
-	collectCoverageFrom: [
-		'packages/**/*.{js,jsx}',
-		'!**/{node_modules,lib,coverage,scripts}/**',
-	],
-	coverageDirectory: '<rootDir>/coverage',
 	moduleFileExtensions: ['js', 'jsx', 'json'],
 	setupFiles: ['<rootDir>/tests/util/setupTest.js'],
 	snapshotSerializers: ['enzyme-to-json/serializer'],
