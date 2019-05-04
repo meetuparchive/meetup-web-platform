93ff2818df17286611fa075f8d24619afab04dd8
mmcgahan <mike.mcgahan@gmail.com>
2019-05-04 13:09:19 +1200

    feat: client-side cookie setting implementation

--- packages/mwp-cookie/src/Cookie.jsx
+++ packages/mwp-cookie/src/Cookie.jsx
@@ -2,10 +2,13 @@
 import withSideEffect from 'react-side-effect';
 import jsCookie from 'js-cookie';
 
+// subset of HapiServerStateCookieOptions that will work with Hapi `h.state()` and
+// js-cookie `Cookie.set()`
 type CookieOpts = {
-	value: ?string,
-	ttl?: number,
+	value?: string,
+	ttl?: number, // milliseconds
 	isHttpOnly?: boolean,
+	isSecure?: boolean,
 	path?: string,
 	domain?: string,
 };
@@ -19,6 +22,7 @@ type CookieState = { [name: string]: CookieOpts };
 const defaults = {
 	path: '/',
 	domain: '.meetup.com',
+	isSecure: true,
 };
 
 /*
@@ -35,10 +39,33 @@ const reducePropsToState = (propsList: CookieState): CookieState =>
 		return acc;
 	}, {});
 
+// The client-side side effect handler
+// This will be called on initial render, so any server-supplied cookies will
+// effectively be overwritten on the client with approximately the same information
+// (server may supply 'max-age' while client provides 'expires' value)
 export const handleStateChangeOnClient = (state: CookieState) => {
+	// loop through cookie key-values and set a cookie for each
 	Object.keys(state).forEach(name => {
-		const { value, ...options } = state[name];
-		// determine 'expires' from 'ttl'?
+		const { value, path, domain, isSecure, ttl, isHttpOnly } = state[name];
+		if (isHttpOnly) {
+			// httpOnly cookies are only for server - cannot be handled on client
+			return;
+		}
+
+		// set up cookie attributes argument for js-cookie
+		// https://github.com/js-cookie/js-cookie/blob/master/README.md#cookie-attributes
+		const options = {
+			path,
+			domain,
+			secure: isSecure,
+		};
+		if (ttl) {
+			// js-cookie treats integer `expires` as whole-day values, so we will
+			// convert the millisecond `ttl` value to `Date`, which is supported
+			// for arbitrary times.
+			options.expires = new Date(new Date().getTime() + ttl);
+		}
+
 		jsCookie.set(name, value, options);
 	});
 };
