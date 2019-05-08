d2a050405b86e03ec2328952aa1402222734ac22
Michael McGahan <mike.mcgahan@gmail.com>
2019-05-09 11:16:24 +1200

    docs: add code comments to Cookie.jsx

--- packages/mwp-cookie/src/Cookie.jsx
+++ packages/mwp-cookie/src/Cookie.jsx
@@ -1,7 +1,24 @@
 // @flow
 import React from 'react';
 
+/**
+ * A React-Context-based universal cookie getter/setter.
+ *
+ * exported React components:
+ * - CookieProvider
+ * - SetCookie
+ * - GetCookie
+ */
+
 type CookieMap = { [string]: string };
+
+/**
+ * These cookie configuration options are a subset of the option params
+ * used by Hapi for
+ * [the cookie-setting `server.state()` function](https://hapijs.com/api#-serverstatename-options).
+ * They can be transformed into analogous params in 'js-cookie' for
+ * [`Cookie.set()`](https://github.com/js-cookie/js-cookie/#cookie-attributes)
+ */
 export type CookieOpts = {
 	ttl?: number | null,
 	isSecure?: boolean,
@@ -13,37 +30,45 @@ export type CookieOpts = {
 type ContextProps = {
 	get: (name?: string) => string | CookieMap,
 	set: (name: string, value: string, opts: CookieOpts) => void,
+	children: React$Node,
 };
-const CookieContext = React.createContext<ContextProps>({
-	set: () => undefined,
-	get: (name?: string) => '',
-});
-export const CookieProvider = (props: ContextProps & { children: React$Node }) => (
-	<CookieContext.Provider value={{ get: props.get, set: props.set }}>
-		{props.children}
-	</CookieContext.Provider>
-);
-
-// subset of HapiServerStateCookieOptions (see flow-typed libdef) that can be
-// used to supply cookie config opts to Hapi `h.state()` and js-cookie `Cookie.set()`
 type SetCookieProps = CookieOpts & {
 	children?: string,
 	name: string,
 };
-const defaults = {
+
+const OPT_DEFAULT = {
 	path: '/',
 	domain: '.meetup.com',
 	isSecure: true,
 	isSameSite: false,
 };
 
-/*
- * This component, when rendered, is solely responsible for triggering side effects
- * that write cookies, both on the server and on the client.
+/**
+ * This context provides the link between the setter/getter routines
+ * supplied by the rendering environment (server/browser) and the internals
+ * of the application - it should not be used directly, but is used to compose
+ * other exported components in this module.
+ */
+const CookieContext = React.createContext<ContextProps>({
+	set: () => undefined,
+	get: (name?: string) => '',
+});
+
+// The context provider - this should generally wrap the entire React application
+export const CookieProvider = (props: ContextProps & { children: React$Node }) => (
+	<CookieContext.Provider value={{ get: props.get, set: props.set }}>
+		{props.children}
+	</CookieContext.Provider>
+);
+
+/**
+ * This component is solely responsible for writing a cookie as
+ * a side effect of rendering, using the `set` function from the CookieContext
  */
 export const SetCookie = (props: SetCookieProps) => {
 	const { name, children, ...options } = props;
-	Object.assign(options, defaults);
+	Object.assign(options, OPT_DEFAULT);
 	return (
 		<CookieContext.Consumer>
 			{Cookie => {
@@ -54,9 +79,11 @@ export const SetCookie = (props: SetCookieProps) => {
 	);
 };
 
-// Simpler consumer that calls children function with the value of
-// the cookie named by props.name
-export const CookieConsumer = (props: {
+/**
+ * Simple cookie context `get` consumer that calls children function with the
+ * value of the cookie named by `props.name`
+ */
+export const GetCookie = (props: {
 	name: string,
 	children: (string | CookieMap) => React$Node,
 }) => (
