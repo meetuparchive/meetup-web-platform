1a309c5b1e0b880a00e815c1beb3fe7138fcf3c6
Oscar Fanelli <oscar.nesis@gmail.com>
2019-06-25 12:45:54 +0200

    chore: support different GTM Environments

--- packages/mwp-app-render/src/util/googleTagManager.test.js
+++ packages/mwp-app-render/src/util/googleTagManager.test.js
@@ -5,8 +5,21 @@ import {
 } from './googleTagManager';
 
 describe('getGoogleTagManagerSnippet()', () => {
+	const MOCK_GTM_KEY = 'GTM-CONTAINER-ID';
+	const MOCK_GTM_PUBLIC_AUTH = 'qwertyuiopasdfghjklzxcvbnm';
+	const MOCK_GTM_PREVIEW_ID = 'env-42';
+
 	it('matches snap', () => {
 		expect(getGoogleTagManagerSnippet()).toMatchSnapshot();
+	});
+	it('matches snap with parameters passed', () => {
+		expect(
+			getGoogleTagManagerSnippet(
+				MOCK_GTM_KEY,
+				MOCK_GTM_PUBLIC_AUTH,
+				MOCK_GTM_PREVIEW_ID
+			)
+		).toMatchSnapshot();
 	});
 });
 
