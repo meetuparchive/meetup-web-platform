01ba22635e8c49c09ea3fd91792d62aeca22b9f6
Oscar Fanelli <ofanelli@meetup.com>
2019-07-04 09:50:23 +0200

    [166870354] feat: Support different GTM Environments (#620)
    * chore: support different GTM Environments
    * Bumped version and updated changelog

--- packages/mwp-app-render/src/util/googleTagManager.js
+++ packages/mwp-app-render/src/util/googleTagManager.js
@@ -1,8 +1,16 @@
 // @flow
 
-// Production and dev keys
-const GTM_KEY =
-	process.env.NODE_ENV === 'production' ? 'GTM-T2LNGD' : 'GTM-W9W847';
+// GTM key for the default GTM container (www.meetup.com)
+const GTM_KEY = 'GTM-T2LNGD';
+
+// public auth key for GTM Environment (production/development)
+const GTM_PUBLIC_AUTH =
+	process.env.NODE_ENV === 'production'
+		? 'KofUFmSzY9oFkO_VAgxnWA'
+		: 'OtzciJkDXO73MJ5VH2mTsA';
+
+// preview ID for the GTM Environment (production/development)
+const GTM_PREVIEW_ID = process.env.NODE_ENV === 'production' ? 'env-1' : 'env-265';
 
 /**
  * @description Gets dataLayer initialization snippet with initial values provided
@@ -26,12 +34,14 @@ export const gtmPush = (data: { [string]: string }) => {
 /**
  * @description Gets google tag manager JS snippet
  * @see {@link https://developers.google.com/tag-manager/quickstart}
-*/
-export const getGoogleTagManagerSnippet = (): string =>
-	`(function(w,d,s,l,i){
-		w[l]=w[l]||[];
-		w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
-		var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-		j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-		f.parentNode.insertBefore(j,f);
-	})(window,document,'script','dataLayer','${GTM_KEY}');`;
+ */
+export const getGoogleTagManagerSnippet = (
+	gtmKey: string = GTM_KEY,
+	gtmPublicAuth: string = GTM_PUBLIC_AUTH,
+	gtmPreviewId: string = GTM_PREVIEW_ID
+): string =>
+	`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl+ '&gtm_auth=${gtmPublicAuth}&gtm_preview=${gtmPreviewId}&gtm_cookies_win=x';f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${gtmKey}');`;
