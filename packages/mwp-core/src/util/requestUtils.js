b3b77a8986585472b8e9214364dfe16930af57db
mmcgahan <mike.mcgahan@gmail.com>
2019-08-17 10:29:51 +1200

    chore: allow querystring override for dev

--- packages/mwp-core/src/util/requestUtils.js
+++ packages/mwp-core/src/util/requestUtils.js
@@ -21,17 +21,26 @@ export const getRemoteIp = (request: HapiRequest): ?string => {
 };
 
 /**
- * Returns a client geo location provided by Fastly.
+ * Returns a client geo location map derived from request properties
  *
- * @param request a request
- * @returns {GeoLocation} a client geo location
+ * Usually, the parameters are supplied by Fastly custom headers
+ *
+ * - `x-region`: a string in the format '[countrycode]/[regioncode]'
+ * - `x-fastly-geo-city': city name as determined by Fastly
+ * - `x-fastly-geo-latlon: string of '[latitude],[longitude]' floats
+ *
+ * Alternatively, override these value (or supply in dev) using querystring params
+ *
+ * - `__geo-region`
+ * - `__geo-city`
+ * - `__geo-latlon`
  */
 export const getRemoteGeoLocation = (request: HapiRequest): GeoLocation => {
-	const { headers } = request;
+	const { headers, query } = request;
 	const geoLocation: GeoLocation = {};
 
 	// COUNTRY + REGION
-	const location = headers['x-region'] || '';
+	const location = query['__geo-region'] || headers['x-region'] || '';
 	const [country, region] = location.split('/');
 	if (country) {
 		geoLocation.country = country;
@@ -41,13 +50,17 @@ export const getRemoteGeoLocation = (request: HapiRequest): GeoLocation => {
 	}
 
 	// CITY
-	const city = headers['x-geo-city'];
+	const city = query['__geo-city'] || headers['x-fastly-geo-city'];
 	if (city) {
 		geoLocation.city = city;
 	}
 
 	// LATITUDE/LONGITUDE
-	const [lat = NaN, lon = NaN] = (headers['x-geo-latlon'] || '')
+	const [lat = NaN, lon = NaN] = (
+		query['__geo-latlon'] ||
+		headers['x-fastly-geo-latlon'] ||
+		''
+	)
 		.split(',')
 		.map(parseFloat);
 	if (!Number.isNaN(lat) && !Number.isNaN(lon)) {
--- packages/mwp-core/src/util/requestUtils.test.js
+++ packages/mwp-core/src/util/requestUtils.test.js
@@ -82,13 +82,38 @@ describe('getRemoteGeoLocation', () => {
 			{ 'x-region': '/ny' },
 			{ region: 'ny' },
 		],
-		['city for x-geo-city', { 'x-geo-city': 'nyc' }, { city: 'nyc' }],
 		[
-			'latlon for x-geo-latlon',
-			{ 'x-geo-latlon': '1.2345,2.3456' },
+			'city for x-fastly-geo-city',
+			{ 'x-fastly-geo-city': 'nyc' },
+			{ city: 'nyc' },
+		],
+		[
+			'latlon for x-fastly-geo-latlon',
+			{ 'x-fastly-geo-latlon': '1.2345,2.3456' },
 			{ latlon: [1.2345, 2.3456] },
 		],
 	])('%s', (_, headers, expected) => {
 		expect(getRemoteGeoLocation({ ...REQUEST_MOCK, headers })).toEqual(expected);
+	});
+	it('overrides all values using querystring', () => {
+		const headers = {
+			'x-region': 'header-country/header-region',
+			'x-fastly-geo-city': 'header-city',
+			'x-fastly-geo-latlon': '9.8765,8.76584',
+		};
+		const query = {
+			'__geo-region': 'query-country/query-region',
+			'__geo-city': 'query-city',
+			'__geo-latlon': '1.2345,2.3456',
+		};
+		const expected = {
+			country: 'query-country',
+			region: 'query-region',
+			city: 'query-city',
+			latlon: [1.2345, 2.3456],
+		};
+		expect(getRemoteGeoLocation({ ...REQUEST_MOCK, headers, query })).toEqual(
+			expected
+		);
 	});
 });
