26250066ddd076fbffe8958291a207ad9fcb5541
mmcgahan <mike.mcgahan@gmail.com>
2019-11-12 09:19:31 +1300

    docs: explain click-track cookie blacklist

--- packages/mwp-api-proxy-plugin/src/util/send.js
+++ packages/mwp-api-proxy-plugin/src/util/send.js
@@ -9,7 +9,11 @@ import config from 'mwp-config';
 
 export const API_META_HEADER = 'X-Meta-Request-Headers';
 const FULL_URL_PATTERN = /^https?:\/\//;
-export const COOKIE_BLACKLIST = ['click-track'];
+
+// some cookies are only consumed by the API proxy server and should not be forwarded
+export const COOKIE_BLACKLIST = [
+	'click-track', // click-track is a potentially large cookie that gets consumed by the mwp-tracking-plugin clickReader module
+];
 
 // create a promisified version of `externalRequest` - can't use `util.promisify`
 // because the callback gets 2 additional arguments, and promisify only supports 1.
